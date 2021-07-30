ece9efd1908f9cdb3195fc9caab02e868e1464cf
dan7sc <su7cheng@gmail.com>
2021-07-30 09:59:42 -0300

    Adiciona appbar a pagina de inserir receita

--- lib/src/features/income/income_page.dart
+++ lib/src/features/income/income_page.dart
@@ -11,6 +11,51 @@ class _IncomePageState extends State<IncomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(189),
+        child: Container(
+          height: 189,
+          color: Colors.blue,
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 49,
+                  bottom: 115,
+                  left: 16,
+                  right: 319,
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.menu),
+                  color: Colors.white,
+                  iconSize: 25,
+                  onPressed: () {},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 95,
+                  bottom: 62,
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Entrada",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: 0.25,
+                      // height: 32,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Center(
         child: Text("Income"),
       ),
