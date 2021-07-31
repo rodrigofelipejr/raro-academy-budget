b00d5563afa20136ece4bde9944964fda1e9ae88
dan7sc <su7cheng@gmail.com>
2021-07-31 16:48:54 -0300

    Adiciona o botao de inserir no fundo central do card

--- lib/src/features/income/income_page.dart
+++ lib/src/features/income/income_page.dart
@@ -68,83 +68,129 @@ class _IncomePageState extends State<IncomePage> {
           right: 16,
           bottom: 40,
         ),
-        child: Card(
-          color: Colors.white,
-          elevation: 3.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(7.0),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 54,
-              right: 54,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 55,
-                    bottom: 12,
-                  ),
-                  child: CustomTextField(
-                    hintText: "Valor em R\$",
-                    labelText: "Valor",
-                    focusNode: _incomeFocusNode,
-                  ),
+        child: Stack(
+          children: [
+            Card(
+              color: Colors.white,
+              elevation: 3.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(7.0),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 12,
-                    bottom: 12,
-                  ),
-                  child: CustomTextField(
-                    hintText: "Tipo de entrada",
-                    labelText: "Tipo de entrada",
-                    focusNode: _inputTypeFocusNode,
-                  ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 54,
+                  right: 54,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 12,
-                    bottom: 12,
-                  ),
-                  child: CustomTextField(
-                    hintText: "Nome da entrada",
-                    labelText: "Nome da entrada",
-                    focusNode: _inputNameFocusNode,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 12,
-                    bottom: 97,
-                  ),
-                  child: TextButton(
-                    onPressed: () => showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(1900),
-                      lastDate: DateTime(2050),
-                    ),
-                    child: Text(
-                      DateFormat("dd/MM/yyyy").format(DateTime.now()).toString(),
-                      style: TextStyle(
-                        fontFamily: "Roboto",
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14.0,
-                        letterSpacing: 0.4,
-                        color: Colors.purple,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 55,
+                        bottom: 12,
+                      ),
+                      child: CustomTextField(
+                        hintText: "Valor em R\$",
+                        labelText: "Valor",
+                        focusNode: _incomeFocusNode,
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 12,
+                        bottom: 12,
+                      ),
+                      child: CustomTextField(
+                        hintText: "Tipo de entrada",
+                        labelText: "Tipo de entrada",
+                        focusNode: _inputTypeFocusNode,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 12,
+                        bottom: 12,
+                      ),
+                      child: CustomTextField(
+                        hintText: "Nome da entrada",
+                        labelText: "Nome da entrada",
+                        focusNode: _inputNameFocusNode,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 12,
+                        bottom: 97,
+                      ),
+                      child: TextButton(
+                        onPressed: () => showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1900),
+                          lastDate: DateTime(2050),
+                        ),
+                        child: Text(
+                          DateFormat("dd/MM/yyyy")
+                              .format(DateTime.now())
+                              .toString(),
+                          style: TextStyle(
+                            fontFamily: "Roboto",
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14.0,
+                            letterSpacing: 0.4,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 429,
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 123,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(24.0),
+                    ),
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          semanticLabel: "INSERIR",
+                        ),
+                        Text(
+                          "INSERIR",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.46,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
