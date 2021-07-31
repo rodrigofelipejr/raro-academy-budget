5377512748d7d3001bb441e4f959a95576b08931
R-A-S-E <rafael.ebrahim@gmail.com>
2021-07-31 10:18:18 -0300

    adicionado card e valor total e button flutuante

--- lib/src/features/day by day/day_page.dart
+++ lib/src/features/day by day/day_page.dart
@@ -13,8 +13,16 @@ class _DayPageState extends State<DayPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          size: 28,
+        ),
+        onPressed: () {},
+      ),
       appBar: AppBar(
-        toolbarHeight: 160,
+        toolbarHeight: MediaQuery.of(context).size.height * 0.22,
         actions: [
           Padding(
             padding: const EdgeInsets.all(16.0),
@@ -89,6 +97,59 @@ class _DayPageState extends State<DayPage> {
             ],
           ),
         ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              elevation: 2,
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.544,
+                    child: ListView(
+                      children: [
+                        ListTile(
+                          title: Text('aloca'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    height: 1,
+                  ),
+                  Container(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height * 0.1,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total entradas',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color.fromARGB(255, 52, 48, 144)),
+                            ),
+                            Text(
+                              '+R\$ 2.415,00',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color.fromARGB(255, 88, 179, 104)),
+                            ),
+                          ],
+                        ),
+                      ))
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
