94bf2cc0955758ea175d83c5b3b8f985f693dd34
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-08-01 17:57:12 -0300

    refatora botao de exibir saldo geral

---
+++ lib/src/features/home/widgets/general_balance/button_icon_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../../../shared/constants/constants.dart';
+
+class ButtonIconShowBalanceWidget extends StatelessWidget {
+  final void Function() onTap;
+  final bool showing;
+
+  const ButtonIconShowBalanceWidget({
+    Key? key,
+    required this.onTap,
+    required this.showing,
+  }) : super(key: key);
+
+  IconData get icon => showing ? Icons.visibility_off : Icons.visibility;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: SizedBox(
+        height: kMinInteractiveDimension,
+        width: kMinInteractiveDimension,
+        child: IconButton(
+          onPressed: onTap,
+          icon: Icon(
+            icon,
+            color: AppColors.roxo,
+          ),
+          splashRadius: kMinInteractiveDimension * 0.5,
+        ),
+      ),
+    );
+  }
+}
