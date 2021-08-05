7e6df77f25683f26ddabe4dd86492c8a5d932da8
dan7sc <su7cheng@gmail.com>
2021-08-05 15:34:21 -0300

    Corrige bug: DatePicker retorna a data corrente quando usuario muda o tipo de entrada

--- lib/src/features/income/date_picker_widget.dart
+++ lib/src/features/income/date_picker_widget.dart
@@ -1,20 +1,62 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_finance_controller/src/features/income/text_styles.dart';
 import 'package:intl/intl.dart';
 
-class DatePickerWidget extends StatelessWidget {
+class DatePickerWidget extends StatefulWidget {
+  DatePickerWidget({
+    Key? key,
+    this.date,
+  }) : super(key: key);
+
+  final DateTime? date;
+
+  @override
+  _DatePickerWidgetState createState() => _DatePickerWidgetState();
+}
+
+class _DatePickerWidgetState extends State<DatePickerWidget> {
+  DateTime? selectedDate;
+  TextEditingController _dateController = TextEditingController();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime? newDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      initialDatePickerMode: DatePickerMode.day,
+      firstDate: DateTime.now().subtract(Duration(days: 365 * 120)),
+      lastDate: DateTime.now().add(Duration(days: 365 * 120)),
+    );
+    if (newDate != null) {
+      selectedDate = newDate;
+      _dateController.text = DateFormat("dd/MM/yyyy").format(selectedDate!);
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    _dateController.text = DateFormat("dd/MM/yyyy").format(DateTime.now());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => showDatePicker(
-        context: context,
-        initialDate: DateTime.now(),
-        firstDate: DateTime.now().subtract(Duration(days: 365 * 120)),
-        lastDate: DateTime.now().add(Duration(days: 365 * 120)),
-      ),
-      child: Text(
-        DateFormat("dd/MM/yyyy").format(DateTime.now()).toString(),
+    return InkWell(
+      onTap: () {
+        _selectDate(context);
+      },
+      child: TextFormField(
+        controller: _dateController,
+        enabled: false,
+        keyboardType: TextInputType.datetime,
         style: TextStyles.blue14w500Roboto,
+        decoration: InputDecoration(
+          disabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+          contentPadding: const EdgeInsets.only(left: 8),
+        ),
       ),
     );
   }
