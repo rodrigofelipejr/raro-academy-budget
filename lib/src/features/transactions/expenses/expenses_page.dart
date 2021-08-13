5760a7b0fd0b2343c854d7128a907eb8d0fadd7b
dan7sc <su7cheng@gmail.com>
2021-08-12 23:04:10 -0300

    Adiciona controllers a ExpensesPage

--- lib/src/features/transactions/expenses/expenses_page.dart
+++ lib/src/features/transactions/expenses/expenses_page.dart
@@ -5,9 +5,11 @@ import '../widgets/appbar_with_drawer.dart';
 import '../widgets/button_widget.dart';
 import '../widgets/date_picker_widget.dart';
 import '../widgets/dropdown_buttom_widget.dart';
-import '../widgets/dropdown_item_data.dart';
 import '../widgets/text_styles.dart';
-import '/src/shared/constants/app_colors.dart';
+import '../repository/transactions_repository.dart';
+import '../controller/transactions_controller.dart';
+import '../controller/date_controller.dart';
+import '../controller/dropdown_controller.dart';
 
 class ExpensesPage extends StatefulWidget {
   const ExpensesPage({Key? key}) : super(key: key);
@@ -17,40 +19,26 @@ class ExpensesPage extends StatefulWidget {
 }
 
 class _ExpensesPageState extends State<ExpensesPage> {
+  TransactionsRepository _repository = TransactionsRepository();
+
+  TransactionsController _controller = TransactionsController();
+  DropdownController _inputTypeController = DropdownController();
+  DateController _dateController = DateController();
+
   FocusNode _expensesFocusNode = FocusNode();
   FocusNode _inputTypeFocusNode = FocusNode();
+  FocusNode _dateFocusNode = FocusNode();
 
-  DropdownItemData? dropdownValue;
-
-  final List<DropdownItemData> list = [
-    DropdownItemData(
-      color: AppColors.amarelo,
-      value: "Refeição",
-    ),
-    DropdownItemData(
-      color: AppColors.verde,
-      value: "Transporte",
-    ),
-    DropdownItemData(
-      color: AppColors.rosa,
-      value: "Viagem",
-    ),
-    DropdownItemData(
-      color: AppColors.azul,
-      value: "Educação",
-    ),
-    DropdownItemData(
-      color: AppColors.roxo,
-      value: "Pagamentos",
-    ),
-    DropdownItemData(
-      color: AppColors.roxo.withOpacity(0.54),
-      value: "Outros",
-    ),
-  ];
+  @override
+  void initState() {
+    _controller.repository = _repository;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
     return Scaffold(
       appBar: AppBarWithDrawer(title: "Saída"),
       drawer: Drawer(),
@@ -73,51 +61,56 @@ class _ExpensesPageState extends State<ExpensesPage> {
               ),
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 54),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 55,
-                        bottom: 12,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 55,
+                          bottom: 12,
+                        ),
+                        child: CustomTextField(
+                          hintText: "Valor",
+                          labelText: "Valor em R\$",
+                          focusNode: _expensesFocusNode,
+                        ),
                       ),
-                      child: CustomTextField(
-                        hintText: "Valor",
-                        labelText: "Valor em R\$",
-                        focusNode: _expensesFocusNode,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Tipo de saída",
+                              style: TextStyles.black12w400RobotoOp54,
+                            ),
+                            DropdownButtomWidget(
+                                value: _inputTypeController.value,
+                                items: _inputTypeController.items,
+                                focusNode: _inputTypeFocusNode,
+                                onChanged: (newValue) {
+                                  _inputTypeController.value = newValue!;
+                                  setState(() {});
+                                }),
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Tipo de saída",
-                            style: TextStyles.black12w400RobotoOp54,
-                          ),
-                          DropdownButtomWidget(
-                              value: dropdownValue,
-                              items: list,
-                              focusNode: _inputTypeFocusNode,
-                              onChanged: (newValue) {
-                                setState(() {
-                                  dropdownValue = newValue as DropdownItemData;
-                                });
-                              }),
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 12,
+                          bottom: 97,
+                        ),
+                        child: DatePickerWidget(
+                          controller: _dateController,
+                          focusNode: _dateFocusNode,
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 12,
-                        bottom: 97,
-                      ),
-                      child: DatePickerWidget(),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -127,7 +120,10 @@ class _ExpensesPageState extends State<ExpensesPage> {
               ),
               child: Align(
                 alignment: Alignment.center,
-                child: ButtonWidget(),
+                child: ButtonWidget(
+                  label: "INSERIR",
+                  onPressed: () {},
+                ),
               ),
             ),
           ],
