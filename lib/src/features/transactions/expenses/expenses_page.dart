8c97f5a093f0dd023fcd36c2f632158d1d3d064e
dan7sc <su7cheng@gmail.com>
2021-08-13 16:42:51 -0300

    Fixa botao de inserir no meio da borda de fundo do card

--- lib/src/features/transactions/expenses/expenses_page.dart
+++ lib/src/features/transactions/expenses/expenses_page.dart
@@ -1,3 +1,4 @@
+import 'package:budget/src/features/transactions/models/transaction_data.dart';
 import 'package:budget/src/shared/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -22,12 +23,17 @@ class _ExpensesPageState extends State<ExpensesPage> {
   TransactionsRepository _repository = TransactionsRepository();
 
   TransactionsController _controller = TransactionsController();
+  TextEditingController _expensesController = TextEditingController();
   DropdownController _inputTypeController = DropdownController();
   DateController _dateController = DateController();
 
   FocusNode _expensesFocusNode = FocusNode();
   FocusNode _inputTypeFocusNode = FocusNode();
   FocusNode _dateFocusNode = FocusNode();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  late TransactionData _data;
 
   @override
   void initState() {
@@ -37,96 +43,128 @@ class _ExpensesPageState extends State<ExpensesPage> {
 
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
     return Scaffold(
       appBar: AppBarWithDrawer(title: "Saída"),
       drawer: Drawer(),
       body: SingleChildScrollView(
-        padding: EdgeInsets.only(
-          top: 16,
-          left: 16,
-          right: 16,
-          bottom: 40,
-        ),
-        child: Stack(
-          children: [
-            Card(
-              color: Colors.white,
-              elevation: 3.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(7.0),
+        padding: EdgeInsets.all(16.0),
+        child: Container(
+          height: 400,
+          child: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Card(
+                color: Colors.white,
+                elevation: 3.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(7.0),
+                  ),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 54),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 55,
-                          bottom: 12,
+                child: Container(
+                  height: 370,
+                  padding: const EdgeInsets.symmetric(horizontal: 54),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 55,
+                            bottom: 12,
+                          ),
+                          child: CustomTextField(
+                            hintText: "Valor",
+                            labelText: "Valor em R\$",
+                            focusNode: _expensesFocusNode,
+                            controller: _expensesController,
+                          ),
                         ),
-                        child: CustomTextField(
-                          hintText: "Valor",
-                          labelText: "Valor em R\$",
-                          focusNode: _expensesFocusNode,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Tipo de saída",
+                                style: TextStyles.black12w400RobotoOp54,
+                              ),
+                              DropdownButtomWidget(
+                                  value: _inputTypeController.value,
+                                  items: _inputTypeController.items,
+                                  focusNode: _inputTypeFocusNode,
+                                  onChanged: (newValue) {
+                                    _inputTypeController.value = newValue!;
+                                    setState(() {});
+                                  }),
+                            ],
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Tipo de saída",
-                              style: TextStyles.black12w400RobotoOp54,
-                            ),
-                            DropdownButtomWidget(
-                                value: _inputTypeController.value,
-                                items: _inputTypeController.items,
-                                focusNode: _inputTypeFocusNode,
-                                onChanged: (newValue) {
-                                  _inputTypeController.value = newValue!;
-                                  setState(() {});
-                                }),
-                          ],
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 12,
+                          ),
+                          child: DatePickerWidget(
+                            controller: _dateController,
+                            focusNode: _dateFocusNode,
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 12,
-                          bottom: 97,
-                        ),
-                        child: DatePickerWidget(
-                          controller: _dateController,
-                          focusNode: _dateFocusNode,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 345,
-              ),
-              child: Align(
-                alignment: Alignment.center,
+              Positioned(
+                bottom: 0,
                 child: ButtonWidget(
                   label: "INSERIR",
-                  onPressed: () {},
+                  onPressed: () {
+                    FocusScope.of(context).unfocus();
+                    if (_formKey.currentState!.validate()) {
+                      print("I***I: ${_expensesController.value.text}");
+                      _data = TransactionData(
+                        value: double.parse(_expensesController.value.text),
+                        type: _inputTypeController.value!.value,
+                        date: _dateController.date,
+                        uid: 'asdfg',
+                      );
+                      // _controller.repository.deleteTransaction("9vSc8LJDgKl2wy4cNGjz");
+                      // _controller.repository.createTransaction(_data);
+                      print('DATA ${_data.toString()}');
+                      _controller.repository.getTransactions();
+
+                      //Adiciona nova transacao na lista
+                      // final list = Modular.get<DailyStore>().transactions();
+                      // list.add(_data);
+                      // Modular.get<DailyStore>().setTransactions();
+                      // Modular.to.pushReplacementNamed(AppRoutes.daily);
+                      showDialog(
+                        context: context,
+                        builder: (_) => Container(
+                          height: 400,
+                          alignment: Alignment.center,
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                "Dados enviados com sucesso",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  },
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
