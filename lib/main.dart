0df0508d0d9cc539f1b5e738ac0d1765b2ccacb7
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-08-15 15:45:52 -0300

    fixa orientação da tela em modo portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'src/app_firebase.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(AppFirebase());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then(
+    (_) => runApp(AppFirebase()),
+  );
 }
