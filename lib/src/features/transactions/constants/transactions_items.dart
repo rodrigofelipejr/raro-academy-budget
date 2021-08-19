1d8ba1f5a43779a35850b8a62ecad4b685376bc2
dan7sc <su7cheng@gmail.com>
2021-08-19 11:56:50 -0300

    Adiciona atributo key a DropdownItemData

--- lib/src/features/transactions/constants/transactions_items.dart
+++ lib/src/features/transactions/constants/transactions_items.dart
@@ -6,22 +6,27 @@ class TransactionsItems {
     DropdownItemData(
       color: AppColors.azul,
       value: "Dinheiro",
+      key: "money",
     ),
     DropdownItemData(
       color: AppColors.azul,
       value: "Pix",
+      key: "pix",
     ),
     DropdownItemData(
       color: AppColors.azul,
       value: "Doc",
+      key: "doc",
     ),
     DropdownItemData(
       color: AppColors.azul,
       value: "Ted",
+      key: "ted",
     ),
     DropdownItemData(
       color: AppColors.azul,
       value: "Boleto",
+      key: "ticket",
     ),
   ];
 
@@ -29,26 +34,32 @@ class TransactionsItems {
     DropdownItemData(
       color: AppColors.amarelo,
       value: "Refeição",
+      key: "meal",
     ),
     DropdownItemData(
       color: AppColors.verde,
       value: "Transporte",
+      key: "transport",
     ),
     DropdownItemData(
       color: AppColors.rosa,
       value: "Viagem",
+      key: "trip",
     ),
     DropdownItemData(
       color: AppColors.ciano,
       value: "Educação",
+      key: "education",
     ),
     DropdownItemData(
       color: AppColors.roxo,
       value: "Pagamentos",
+      key: "payments",
     ),
     DropdownItemData(
       color: AppColors.gray,
       value: "Outros",
+      key: "others",
     ),
   ];
 }
--- lib/src/features/transactions/widgets/dropdown_item_data.dart
+++ lib/src/features/transactions/widgets/dropdown_item_data.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class DropdownItemData {
   final Color color;
   final String value;
+  final String key;
 
   DropdownItemData({
     required this.color,
     required this.value,
+    required this.key,
   });
 }
