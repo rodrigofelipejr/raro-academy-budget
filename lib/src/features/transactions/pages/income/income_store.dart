64290564be97734c3212c8b3f058967ea7511d57
dan7sc <su7cheng@gmail.com>
2021-08-20 16:14:29 -0300

    Adiciona operacoes CRUD em IncomeStore

--- lib/src/features/transactions/pages/income/income_store.dart
+++ lib/src/features/transactions/pages/income/income_store.dart
@@ -1,16 +1,51 @@
 import 'package:budget/src/features/transactions/repositories/transactions_repository.dart';
 import 'package:budget/src/shared/models/models.dart';
+import 'package:mobx/mobx.dart';
 
-class IncomeStore {
+import '/src/shared/stores/auth/auth_store.dart';
+import '/src/shared/stores/stores.dart';
+
+part 'income_store.g.dart';
+
+class IncomeStore = _IncomeStoreBase with _$IncomeStore;
+
+abstract class _IncomeStoreBase extends BaseStore with Store {
   final TransactionsRepository repository;
+  final AuthStore authStore;
 
-  IncomeStore(this.repository);
+  _IncomeStoreBase(this.repository, this.authStore);
 
-  Future<bool> createTransaction({
+  @override
+  Future<void> init() async {}
+
+  Future<String?> createTransaction({
+    required TransactionModel transaction,
+  }) async {
+      transaction =
+        transaction.copyWith(uuid: authStore.firebaseAuth.currentUser!.uid);
+    try {
+      return await repository.createTransaction(transaction);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<bool> updateTransaction({
     required TransactionModel transaction,
   }) async {
     try {
-      await repository.createTransaction(transaction);
+      await repository.updateTransaction(transaction);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> deleteTransaction({
+    required TransactionModel transaction,
+  }) async {
+    try {
+      await repository.deleteTransaction(transaction);
       return true;
     } catch (e) {
       return false;
