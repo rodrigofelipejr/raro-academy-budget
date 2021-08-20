2ae1ff6750505b56629e07c272d791b9be6dd6cc
dan7sc <su7cheng@gmail.com>
2021-08-20 16:15:51 -0300

    Refatora TransactionRepository

--- lib/src/features/transactions/repositories/transaction_repository_interface.dart
+++ lib/src/features/transactions/repositories/transaction_repository_interface.dart
@@ -2,9 +2,9 @@ import 'package:budget/src/shared/models/models.dart';
 
 abstract class ITransactionsRepository {
   Future<void> createTransaction(TransactionModel transaction);
-  Future<void> updateTransaction(TransactionModel transaction, String docId);
-  Future<void> deleteTransaction(String uuid);
-  Future<List<TransactionModel>> getTransactions();
+  Future<void> updateTransaction(TransactionModel transaction);
+  Future<void> deleteTransaction(TransactionModel transaction);
+  Future<List<TransactionModel>> getTransactions(String uuid);
   Future<List<TransactionModel>> getTransactionsByMonth({required String uuid ,required int month});
   Future<void> showTransactions();
   Future<void> showDocs();
--- lib/src/features/transactions/repositories/transactions_repository.dart
+++ lib/src/features/transactions/repositories/transactions_repository.dart
@@ -13,52 +13,69 @@ class TransactionsRepository implements ITransactionsRepository {
   TransactionsRepository(this.firestore);
 
   @override
-  Future<void> createTransaction(TransactionModel transaction) async {
-    await FirebaseFirestore.instance.collection(_collectionPath).add(transaction.toMap());
+  Future<String?> createTransaction(TransactionModel transaction) async {
+    CollectionReference db =
+        FirebaseFirestore.instance.collection(_collectionPath);
+    final newTransaction = await db.add(transaction.toMap());
+    return newTransaction.id;
   }
 
   @override
-  Future<void> deleteTransaction(String uuid) async {
-    await FirebaseFirestore.instance.collection(_collectionPath).doc(uuid).delete();
+  Future<void> updateTransaction(TransactionModel transaction) async {
+    CollectionReference db =
+        FirebaseFirestore.instance.collection(_collectionPath);
+    db.doc(transaction.id).set(transaction.toMap(), SetOptions(merge: true));
   }
 
   @override
-  Future<List<TransactionModel>> getTransactions() async {
+  Future<void> deleteTransaction(TransactionModel transaction) async {
+    CollectionReference db =
+        FirebaseFirestore.instance.collection(_collectionPath);
+    db.doc(transaction.id).delete();
+  }
+
+  @override
+  Future<List<TransactionModel>> getTransactions(String uuid) async {
     CollectionReference db =
         FirebaseFirestore.instance.collection(_collectionPath);
     final snapshot = await db
-        .where('uuid', isEqualTo: AppSettings.userUuid)
-        .orderBy('createAt', descending: true)
+        .where('uuid', isEqualTo: uuid)
+        .orderBy('updateAt', descending: true)
         .get();
 
     return snapshot.docs.map((e) => TransactionModel.fromFirestore(e)).toList();
   }
 
   @override
-  Future<List<TransactionModel>> getTransactionsByMonth({required int month, required String uuid}) async {
-    CollectionReference db = FirebaseFirestore.instance.collection(_collectionPath);
+  Future<List<TransactionModel>> getTransactionsByMonth(
+      {required int month, required String uuid}) async {
+    CollectionReference db =
+        FirebaseFirestore.instance.collection(_collectionPath);
     final snapshot = await db
         .where('uuid', isEqualTo: uuid)
-        .where('createAt', isGreaterThanOrEqualTo: Timestamp.fromDate(Dates.firstDayMonth(month: month)))
-        .where('createAt', isLessThanOrEqualTo: Timestamp.fromDate(Dates.lastDayMonth(month: month)))
+        .where('createAt',
+            isGreaterThanOrEqualTo:
+                Timestamp.fromDate(Dates.firstDayMonth(month: month)))
+        .where('createAt',
+            isLessThanOrEqualTo:
+                Timestamp.fromDate(Dates.lastDayMonth(month: month)))
         .orderBy('createAt', descending: true)
         .get();
 
     return snapshot.docs.map((e) => TransactionModel.fromFirestore(e)).toList();
-  }
-
-  @override
-  Future<void> updateTransaction(TransactionModel transaction, String docId) async {
-    await FirebaseFirestore.instance
-        .collection(_collectionPath)
-        .doc(docId)
-        .set(transaction.toMap(), SetOptions(merge: true));
   }
 
   // Para testes
   Future<void> showTransactions() async {
-    final response = await FirebaseFirestore.instance.collection(_collectionPath).get();
-    print('TRANSACTIONS: ${response.docs.map(((e) => e.data()))}');
+    final response = await FirebaseFirestore.instance
+        .collection(_collectionPath)
+        // .where('uuid', isEqualTo: '')
+        .where('uuid', isEqualTo: 'bz6QAEAmPTfAM4baxbTWV62BXou1')
+        // .where('uuid', isEqualTo: '31KaO9IFxTOY3No1kWfoYyHptiw2')
+        .get();
+    response.docs.map((e) => print(TransactionModel.fromFirestore(e)));
+    print(
+        'TRANSACTIONS: ${response.docs.map((e) => TransactionModel.fromFirestore(e))}');
     print(response);
   }
 
