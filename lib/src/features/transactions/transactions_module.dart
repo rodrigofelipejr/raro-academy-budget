371ff61cd9c96bd89dd0ec16d922da23ad33ef0c
dan7sc <su7cheng@gmail.com>
2021-08-20 16:10:53 -0300

    Injeta dependencia em IncomeStore

--- lib/src/features/transactions/transactions_module.dart
+++ lib/src/features/transactions/transactions_module.dart
@@ -15,7 +15,7 @@ class TransactionsModule extends Module {
   List<Bind<Object>> get binds => [
         Bind.singleton((i) => TransactionsRepository(i())),
         Bind.singleton((i) => TransactionsStore(i(), i(), i())),
-        Bind.singleton((i) => IncomeStore(i())),
+        Bind.singleton((i) => IncomeStore(i(), i())),
         Bind.singleton((i) => ExpensesStore(i())),
         Bind.singleton((i) => FirebaseFirestore.instance),
       ];
