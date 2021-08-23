bbf505cbb8b09ff3116e6b91d0503e9b7a4e97ac
R-A-S-E <rafael.ebrahim@gmail.com>
2021-08-22 23:17:11 -0300

    icon dropdawn buttons

--- lib/src/features/transactions/widgets/dropdown_item_data.dart
+++ lib/src/features/transactions/widgets/dropdown_item_data.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class DropdownItemData {
+  final String image;
   final Color color;
   final String value;
   final String key;
 
   DropdownItemData({
+    required this.image,
     required this.color,
     required this.value,
     required this.key,
